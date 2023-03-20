7577259896a5aae14dbce1d47af376abb16d2a3b
Darius Fang <56571687+DarFang@users.noreply.github.com>
2023-03-20 09:05:41 -0600

    added 2 trackers (can be done to scale)

--- CSTracker.py
+++ CSTracker.py
@@ -2,27 +2,27 @@ import numpy as np
 import cv2 as cv
 class CSTracker():
     def __init__(self, ID, pts, color, initFrame):
+        self.dst = []
         self.ID = ID
         self.pts = pts
         print(self.pts)
-        self.color = color
         x = pts[0]
         y = pts[1]
         w, h = pts[2]-x, pts[3]-y
         self.track_window = (x, y, w, h)
         roi = initFrame[y:y+h, x:x+w]
         hsv_roi =  cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+        mask = cv.inRange(hsv_roi, np.array((color[0], color[2],color[3])), np.array((color[1],255,255)))
         self.roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
         cv.normalize(self.roi_hist,self.roi_hist,0,255,cv.NORM_MINMAX)
         # Setup the termination criteria, either 10 iteration or move by at least 1 pt
-        self.term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
+        self.term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 3 )
         self.center = []
     
     def updatePoints(self, hsv):
-        dst = cv.calcBackProject([hsv],[0],self.roi_hist,[0,180],1)
+        self.dst = cv.calcBackProject([hsv],[0],self.roi_hist,[0,180],1)
         # apply camshift to get the new location
-        ret, self.track_window = cv.CamShift(dst, self.track_window,self. term_crit)
+        ret, self.track_window = cv.CamShift(self.dst, self.track_window,self. term_crit)
         # Draw it on image
         self.pts = cv.boxPoints(ret)
         self.pts = np.int0(self.pts)
@@ -35,3 +35,4 @@ class CSTracker():
         if ID:
             frame = cv.putText(frame, self.ID, (self.center), cv.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 255), 1, cv.LINE_AA)
         return frame
+    
--- camShift.py
+++ camShift.py
@@ -13,26 +13,34 @@ from CSTracker import CSTracker as cst
 # # setup initial location of window
 # x, y, w, h = 300, 200, 100, 50 # simply hardcoded the values
 
-numPoints = 2
+numPoints = 4
 def draw_point(event, x, y, flags, param):
    global Input, arrPoints
    if event == cv.EVENT_LBUTTONDOWN:
       Input = cv.circle(Input, (x,y), radius=5, color=(0, 0, 255), thickness=-1)
       arrPoints.append(np.array([x,y]))
-
+mirror = 1
 
 cam = cv.VideoCapture(0)
+cam.set(cv.CAP_PROP_FRAME_WIDTH, 1920  )
+cam.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
 arrPoints = []
 while True:
     # read frame
     ret, frame = cam.read()
+    if mirror:
+        frame = cv.flip(frame, -1)
     cv.imshow("test", frame)
     k = cv.waitKey(1)
     if k%256 == 27:
     # ASCII:ESC pressed, exit
         print("Escape hit, closing...")
         break
+    if k == ord('m'):
+        mirror = not mirror
 ret, Input = cam.read()
+if mirror:
+    Input = cv.flip(Input, -1)
 cv.namedWindow("test")
 cv.setMouseCallback("test", draw_point)
 while len(arrPoints)<numPoints:
@@ -41,18 +49,30 @@ while len(arrPoints)<numPoints:
     k = cv.waitKey(1)
     if k%256 == 27:
         break
-
-t1 = cst("red", [*arrPoints[0], *arrPoints[1]],(180,255,255),  frame)
+    if k == ord('m'):
+        mirror = not mirror
+# green 31 79 100 20 
+# red 0 10 100 20 
+t1 = cst("red", [*arrPoints[0], *arrPoints[1]],[0,10,100, 20],  frame)
+t2 = cst("green", [*arrPoints[2], *arrPoints[3]],[31,79,100, 20],  frame)
 while(1):
     ret, frame = cam.read()
+    if mirror:
+        frame = cv.flip(frame, -1)
     if ret == True:
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         t1.updatePoints(hsv)
         frame = t1.drawPoly(frame)
         frame = t1.drawPt(frame, True)
+        t2.updatePoints(hsv)
+        frame = t2.drawPoly(frame)
+        frame = t2.drawPt(frame, True)
         cv.imshow("frame", frame)
+        cv.imshow("hist", t1.dst)
         k = cv.waitKey(30) & 0xff
         if k == 27:
             break
+        if k == ord('m'):
+            mirror = not mirror
     else:
         break
